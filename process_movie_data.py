f1dad60034c541622723c3b87ed7883def352caf
Trinity <tzhang@g.hmc.edu>
2023-10-21 22:59:22 -0700

    updated to top ten instead of top 5.

--- process_movie_data.py
+++ process_movie_data.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
-# A script that imports movie data and finds the top-5 highest grossing movies
+# A script that imports movie data and finds the top-10 highest grossing movies
 import csv
 
 
-def find_top_5(filename):
-    """Finds the top 5 highest grossing movies in a CSV dataset.
+def find_top_10(filename):
+    """Finds the top 10 highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Output: None
        Effect: should print five lines of text
@@ -17,15 +17,15 @@ def find_top_5(filename):
     # Reformat some data types
     for row in rows:
         row["Gross"] = int(row["Gross"])
-        row["Year"] = int(row["Release Date"][:4])
+        row["Year"] = int(row["Release Date"][:9])
 
-    # Sort data and get top 5
+    # Sort data and get top 10
     gross_sort = lambda x : x["Gross"]
     rows.sort(key=gross_sort)
-    top_five = rows[:-6:-1]
+    top_ten = rows[:-11:-1]
 
     # Print out results
-    for i, row in enumerate(top_five):
+    for i, row in enumerate(top_ten):
         print("{ind}. {row[Title]} ({row[Year]}) - ${row[Gross]:,d}".format(
             ind=i+1,
             row=row))
@@ -35,4 +35,5 @@ def find_top_5(filename):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
-    find_top_5("Movies_gross_rating.csv")
+    find_top_10("Movies_gross_rating.csv")
+
